49d07a6bcb754e0034bbf583510de641311fd53d
Michael Raafat <113734623+Michael-raa@users.noreply.github.com>
2026-07-05 21:20:56 +0300

    Update Mysql project.sql

--- Mysql project.sql
+++ Mysql project.sql
@@ -2,8 +2,6 @@ SELECT *
 FROM world_layoffs.layoffs;
 
 
-
--- first thing we want to do is create a staging table. This is the one we will work in and clean the data. We want a table with the raw data in case something happens
 CREATE TABLE world_layoffs.layoffs_staging 
 LIKE world_layoffs.layoffs;
 
@@ -56,12 +54,11 @@ Delete
 from layoffs_staging2
 where row_num > 1;
 
--- 2. Standardize Data
 
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
 
--- if we look at industry it looks like we have some null and empty rows, let's take a look at these
+
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
@@ -72,26 +69,20 @@ WHERE industry IS NULL
 OR industry = ''
 ORDER BY industry;
 
--- let's take a look at these
+
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE company LIKE 'Bally%';
--- nothing wrong here
+
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE company LIKE 'airbnb%';
 
--- it looks like airbnb is a travel, but this one just isn't populated.
--- I'm sure it's the same for the others. What we can do is
--- write a query that if there is another row with the same company name, it will update it to the non-null industry values
--- makes it easy so if there were thousands we wouldn't have to manually check them all
 
--- we should set the blanks to nulls since those are typically easier to work with
 UPDATE world_layoffs.layoffs_staging2
 SET industry = NULL
 WHERE industry = '';
 
--- now if we check those are all null
 
 SELECT *
 FROM world_layoffs.layoffs_staging2
@@ -99,7 +90,7 @@ WHERE industry IS NULL
 OR industry = ''
 ORDER BY industry;
 
--- now we need to populate those nulls if possible
+
 
 UPDATE layoffs_staging2 t1
 JOIN layoffs_staging2 t2
@@ -108,7 +99,7 @@ SET t1.industry = t2.industry
 WHERE t1.industry IS NULL
 AND t2.industry IS NOT NULL;
 
--- and if we check it looks like Bally's was the only one without a populated row to populate this null values
+
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE industry IS NULL 
@@ -117,7 +108,7 @@ ORDER BY industry;
 
 -- ---------------------------------------------------
 
--- I also noticed the Crypto has multiple different variations. We need to standardize that - let's say all to Crypto
+
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
@@ -126,18 +117,18 @@ UPDATE layoffs_staging2
 SET industry = 'Crypto'
 WHERE industry IN ('Crypto Currency', 'CryptoCurrency');
 
--- now that's taken care of:
+
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
 
 -- --------------------------------------------------
--- we also need to look at 
+
 
 SELECT *
 FROM world_layoffs.layoffs_staging2;
 
--- everything looks good except apparently we have some "United States" and some "United States." with a period at the end. Let's standardize this.
+
 SELECT DISTINCT country
 FROM world_layoffs.layoffs_staging2
 ORDER BY country;
@@ -145,21 +136,21 @@ ORDER BY country;
 UPDATE layoffs_staging2
 SET country = TRIM(TRAILING '.' FROM country);
 
--- now if we run this again it is fixed
+
 SELECT DISTINCT country
 FROM world_layoffs.layoffs_staging2
 ORDER BY country;
 
 
--- Let's also fix the date columns:
+
 SELECT *
 FROM world_layoffs.layoffs_staging2;
 
--- we can use str to date to update this field
+
 UPDATE layoffs_staging2
 SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
 
--- now we can convert the data type properly
+
 ALTER TABLE layoffs_staging2
 MODIFY COLUMN `date` DATE;
 
@@ -169,19 +160,6 @@ FROM world_layoffs.layoffs_staging2;
 
 
 
-
-
--- 3. Look at Null Values
-
--- the null values in total_laid_off, percentage_laid_off, and funds_raised_millions all look normal. I don't think I want to change that
--- I like having them null because it makes it easier for calculations during the EDA phase
-
--- so there isn't anything I want to change with the null values
-
-
-
-
--- 4. remove any columns and rows we need to
 
 SELECT *
 FROM world_layoffs.layoffs_staging2
